87013c9b0252e2dfdba736586f3d9b6eec64ed16
marami52 <marami@ironix.com>
2016-07-08 11:15:46 -0700

    Update bostonhist.R

--- R/bostonhist.R
+++ R/bostonhist.R
@@ -1,6 +1,5 @@
 bostonhist <- function(variable){
 	library(ggplot2)
-	ggplot(Boston, aes(variable)) +
-  	geom_histogram()
-	#hist(Boston[[variable]], xlab=variable, main="Example plot with lazyload data")
+	#ggplot(Boston, aes(variable)) + geom_histogram()
+	hist(Boston[[variable]], xlab=variable, main="Example plot with lazyload data")
 }
